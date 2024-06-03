3d1319773aedfd15b15015c702d11bce9e4a5346
Ortega-juuhl <145920496+Ortega-juuhl@users.noreply.github.com>
2024-06-03 09:13:38 +0200

    more and more bug fixing

--- install_lamp.sh
+++ install_lamp.sh
@@ -51,12 +51,13 @@ pause 10
 
 # Secure MariaDB and create user/database
 sudo mysql -u root <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'admin';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
-SELECT user, authentication_string, plugin, host FROM mysql.user;
 CREATE USER 'root1'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'admin';
 GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'root1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'admin';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
+SELECT user, authentication_string, plugin, host FROM mysql.user;
 EOF
 pause 10
 
