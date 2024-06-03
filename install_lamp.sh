89d322b152abcc57f7137c1a1e43df251a218587
Ortega-juuhl <145920496+Ortega-juuhl@users.noreply.github.com>
2024-06-03 08:32:35 +0200

    added more pause time

--- install_lamp.sh
+++ install_lamp.sh
@@ -13,65 +13,65 @@ sudo apt update
 
 # Install Apache
 sudo apt install -y apache2
-pause 3
+pause 10
 sudo systemctl enable apache2.service
-pause 3
+pause 10
 sudo systemctl status apache2 || { echo "Failed to get Apache status"; exit 1; }
-pause 3
+pause 10
 
 # Configure UFW
 sudo ufw enable
-pause 3
+pause 10
 sudo ufw allow from 10.0.0.0/8 to any port 3306
-pause 3
+pause 10
 sudo ufw allow in on eth0 to any port 80
-pause 3
+pause 10
 sudo ufw allow 'Apache Full'
-pause 3
+pause 10
 sudo ufw allow 'OpenSSH'
-pause 3
+pause 10
 sudo ufw allow from any to any port 20,21,10000:10100 proto tcp
-pause 3
+pause 10
 
 # Install PHP
 sudo apt install -y php
-pause 3
+pause 10
 sudo apt install -y libapache2-mod-php
-pause 3
+pause 10
 sudo apt-cache search libapache2*
-pause 3
+pause 10
 
 # Install OpenSSH server
 sudo apt install -y openssh-server
-pause 3
+pause 10
 sudo nano /etc/ssh/sshd_config
-pause 3
+pause 10
 
 # Install MariaDB
 sudo apt install -y mariadb-server
-pause 3
+pause 10
 sudo systemctl start mariadb.service
-pause 3
+pause 10
 sudo mysql_secure_installation
-pause 3
+pause 10
 
 # Secure MariaDB and create user/database
 sudo mysql <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'IMKuben1337!';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'IMKuben1337!';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'admin';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
 SELECT user,authentication_string,plugin,host FROM mysql.user;
-CREATE USER 'alfmorten'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'IMKuben1337!';
-GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'alfmorten'@'localhost' WITH GRANT OPTION;
+CREATE USER 'root1'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'admin';
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'root1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
-pause 3
+pause 10
 
 # Check MySQL status
 sudo systemctl status mysql.service || { echo "Failed to get MySQL status"; exit 1; }
-pause 3
+pause 10
 
 # Create database
-mysql -u alfmorten -p'IMKuben1337!' <<EOF
+mysql -u root1 -p'admin' <<EOF
 CREATE DATABASE chat_system CHARACTER SET utf8;
 exit
 EOF
